1e3885e3dc41d83b1f79799665b75bb4cc54d086
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-01-09 17:02:50 +0100

    feat: Add ProductBloc provider to manage product-related state

--- lib/core/provider/bloc_provider.dart
+++ lib/core/provider/bloc_provider.dart
@@ -1,11 +1,19 @@
 import 'package:clot/features/auth/data/firebase_auth_repository.dart';
 import 'package:clot/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:clot/features/products/data/firebase_products_repository.dart';
+import 'package:clot/features/products/presentation/bloc/product_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 final appBlocProvider = [
-  BlocProvider(
+  BlocProvider<AuthBloc>(
     create: (context) =>
         AuthBloc(authRepository: FirebaseAuthRepository())
           ..add(AuthStatusChecked()),
+  ),
+  // product
+  BlocProvider<ProductBloc>(
+    create: (context) =>
+        ProductBloc(productRepository: FirebaseProductsRepository())
+          ..add(LoadCategories()),
   ),
 ];
