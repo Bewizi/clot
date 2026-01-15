6e1de11edd13abd747ed0582ab4dd87a71a155a2
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-01-15 05:31:43 +0100

    feat: Relocate reset_password.dart and update import path
    Refactors the file structure by moving `reset_password.dart` into a new `pages` subdirectory within `features/auth/presentation`. The import path in the moved file is updated accordingly to reflect the new location of `sign_in.dart`.

--- lib/features/auth/presentation/pages/forgot_password/reset_password.dart
+++ lib/features/auth/presentation/pages/forgot_password/reset_password.dart
@@ -3,7 +3,7 @@ import 'package:clot/core/presentation/constants/app_images.dart';
 import 'package:clot/core/presentation/ui/extension/app_spacing_extension.dart';
 import 'package:clot/core/presentation/ui/widgets/app_button.dart';
 import 'package:clot/core/presentation/ui/widgets/text_styles.dart';
-import 'package:clot/features/auth/presentation/sign_in/sign_in.dart';
+import 'package:clot/features/auth/presentation/pages/sign_in/sign_in.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
