465ca18c4801f7a0d6cf5c08ff547f661a447477
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-01-22 10:52:26 +0100

    feat: Add CategoryBloc to app providers

--- lib/core/provider/bloc_provider.dart
+++ lib/core/provider/bloc_provider.dart
@@ -2,6 +2,7 @@ import 'package:clot/features/auth/data/firebase_auth_repository.dart';
 import 'package:clot/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:clot/features/products/data/firebase_products_repository.dart';
 import 'package:clot/features/products/presentation/bloc/product_bloc.dart';
+import 'package:clot/features/products/presentation/category_bloc/category_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 final appBlocProvider = [
@@ -15,5 +16,12 @@ final appBlocProvider = [
     create: (context) =>
         ProductBloc(productRepository: FirebaseProductsRepository())
           ..add(LoadCategories()),
+  ),
+
+  BlocProvider<CategoryBloc>(
+    create: (context) => CategoryBloc(
+      // productRepository: FirebaseProductsRepository(),
+      repository: FirebaseProductsRepository(),
+    )..add(LoadAllCategoriesForBrowsing()),
   ),
 ];
