0257607b945bcb3dd5dd0d8cb7041f2989d45389
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-01-23 15:45:46 +0100

    feat: Refactor AppButton and add border support
    The `AppButton` widget is refactored by removing the `LayoutBuilder` and `width` constraint. Horizontal padding has been added. The widget now supports an optional border with customizable color and width.

--- lib/core/presentation/constants/app_images.dart
+++ lib/core/presentation/constants/app_images.dart
@@ -1,4 +1,6 @@
 class AppImages {
   static const String appLogo = 'assets/images/logo.png';
   static const String emailBox = 'assets/images/email_box.png';
+  static const String kBellIcon = 'assets/images/bell.png';
+  static const String kCheckOut = 'assets/images/check-out.png';
 }
