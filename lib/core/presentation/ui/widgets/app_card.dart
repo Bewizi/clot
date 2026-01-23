b9591cea62bcf4f5a440f2f460c2b88c96dc0217
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-01-23 09:42:49 +0100

    feat: Refactor AppCard widget styles and text handling
    This commit refactors the `AppCard` widget by:
    - Removing fixed height and margin.
    - Improving text wrapping and overflow handling for product name and price by setting `maxLines` and `overflow`.
    - Adjusting padding, alignment, and icon positioning for a better layout.

--- lib/core/presentation/ui/widgets/app_card.dart
+++ lib/core/presentation/ui/widgets/app_card.dart
@@ -24,50 +24,61 @@ class AppCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.sizeOf(context).width * 0.35,
-      height: MediaQuery.sizeOf(context).width * 0.18,
-      margin: EdgeInsets.only(right: 16),
       decoration: BoxDecoration(
         color: AppColors.kLightGrey,
         borderRadius: BorderRadius.circular(8),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
         children: [
           Expanded(
             child: Stack(
               children: [
                 ClipRRect(
                   // borderRadius: BorderRadius.circular(10),
-                  child: Image.network(
-                    image,
-                    fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) => Container(
-                      color: AppColors.kBlcak100.withValues(alpha: 0.5),
-                      child: const Icon(
-                        Icons.image_not_supported_outlined,
-                        size: 30,
+                  child: Center(
+                    child: Image.network(
+                      image,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) => Container(
+                        color: AppColors.kBlcak100.withValues(alpha: 0.5),
+                        child: const Icon(
+                          Icons.image_not_supported_outlined,
+                          size: 30,
+                        ),
                       ),
                     ),
                   ),
                 ),
                 Positioned(
                   top: 10,
-                  right: 0,
+                  right: 9,
                   child: SvgPicture.asset(icon, width: 24, height: 24),
                 ),
               ],
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            padding: const EdgeInsets.all(8),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TextRegular(name, color: AppColors.kBlcak100, fontSizes: 14.fs),
-                8.horizontalSpace,
+                TextRegular(
+                  name,
+                  color: AppColors.kBlcak100,
+                  fontSizes: 14.fs,
+                  maxLines: 2,
+                  overFlow: TextOverflow.ellipsis,
+                ),
+                8.verticalSpace,
                 TextSmall(
                   price,
                   color: AppColors.kBlcak100,
                   fontWeight: FontManagerWeight.bold,
+                  maxLines: 1,
+                  overFlow: TextOverflow.ellipsis,
+                  softWrap: false,
                 ),
               ],
             ),
