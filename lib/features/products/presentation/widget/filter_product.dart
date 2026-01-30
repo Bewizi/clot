2cc1885a4b4267127fedee2872b046abb5ddcb07
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-01-30 05:49:19 +0100

    feat: Refactor FilterProduct widget for improved icon handling and tap functionality

--- lib/features/products/presentation/widget/filter_product.dart
+++ lib/features/products/presentation/widget/filter_product.dart
@@ -1,6 +1,6 @@
 import 'package:clot/core/presentation/ui/extension/app_spacing_extension.dart';
 import 'package:clot/core/presentation/ui/widgets/text_styles.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class FilterProduct extends StatelessWidget {
@@ -11,6 +11,8 @@ class FilterProduct extends StatelessWidget {
     this.onTap,
     this.color,
     this.textColor,
+    this.iconColor,
+    this.reverseOrder = false,
   });
 
   final String title;
@@ -18,23 +20,46 @@ class FilterProduct extends StatelessWidget {
   final VoidCallback? onTap;
   final Color? color;
   final Color? textColor;
+  final Color? iconColor;
+  final bool reverseOrder;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
 
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(100),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextSmall(title, color: textColor),
-          if (icon != null) 8.horizontalSpace,
-          if (icon != null) SvgPicture.asset(icon!),
-        ],
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: reverseOrder
+              ? [
+                  if (icon != null)
+                    SvgPicture.asset(
+                      icon!,
+                      colorFilter: iconColor != null
+                          ? ColorFilter.mode(iconColor!, BlendMode.srcIn)
+                          : null,
+                    ),
+                  if (icon != null) 8.horizontalSpace,
+                  TextSmall(title, color: textColor),
+                ]
+              : [
+                  TextSmall(title, color: textColor),
+                  if (icon != null) 8.horizontalSpace,
+                  if (icon != null)
+                    SvgPicture.asset(
+                      icon!,
+                      colorFilter: iconColor != null
+                          ? ColorFilter.mode(iconColor!, BlendMode.srcIn)
+                          : null,
+                    ),
+                ],
+        ),
       ),
     );
   }
