6a940304efc5771e2d1c5fe665f8a0071a3c9210
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-02-10 11:13:15 +0100

    feat: Integrate Device Preview for UI testing
    This commit integrates the `device_preview` package into the application to facilitate UI testing across different devices and screen sizes.
    
    Key changes:
    - The `device_preview` package is added as a dependency.
    - The root `MyApp` widget is wrapped with the `DevicePreview` widget.
    - The `MaterialApp.router` is configured with `DevicePreview.locale(context)` and `DevicePreview.appBuilder` to enable its functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:clot/core/navigation/router.dart';
 import 'package:clot/core/presentation/theme/app_theme.dart';
 import 'package:clot/core/provider/bloc_provider.dart';
 import 'package:clot/firebase_options.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,16 +10,22 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
+  runApp(
+    DevicePreview(enabled: true, builder: (context) => const MyApp()),
+    // MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: appBlocProvider,
       child: MaterialApp.router(
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
         theme: AppThemeData.lightTheme,
         routerConfig: appRoute,
         debugShowCheckedModeBanner: false,
